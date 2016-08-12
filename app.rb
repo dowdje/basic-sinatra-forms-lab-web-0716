a063cd043b9bdea3692cda509ef979f0d6fd7be7
dowdje <dowdje89@gmail.com>
2016-08-12 09:07:53 -0400

    Created basic sinatra forms

--- app.rb
+++ app.rb
@@ -2,5 +2,20 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
+  get '/newteam' do
+    erb :newteam
+  end
+
+  post '/team' do
+    @coach = params['coach']
+    @c = params['c']
+    @name = params['name']
+    @pg = params['pg']
+    @sg = params['sg']
+    @sf = params['sf']
+    @pf = params['pf']
+    erb :team
+  end
+
 
 end
